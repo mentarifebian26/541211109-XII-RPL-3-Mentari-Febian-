f9e01bb76f5f86308576538e6a6280cbb0deb917
mentarifebian26 <mentarifebyan@gmail.com>
2023-11-01 12:45:11 +0700

    Praktek 01-09

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const userrouter = require('./router/users')
+const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+  res.send('JADIIIII')
+})
+ 
+app.use(userrouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
