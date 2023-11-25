6a754532e9b6252dc87515b95a1c0bc4e006e322
mentarifebian26 <mentarifebyan@gmail.com>
2023-11-25 20:33:56 +0700

    Your commit message here

---
+++ router/siswa.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const usercontroller = require('../controllers/siswa')
+
+router.get('/siswa', usercontroller.index)
+
+router.get('/siswa/:id', usercontroller.show)
+
+  router.post('/siswa', usercontroller.store)
+
+  router.put('/siswa/:id', usercontroller.update)
+
+  router.delete('/siswa/:id', usercontroller.delete)
+
+  module.exports = router
